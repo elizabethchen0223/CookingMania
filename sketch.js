5f76f76e03cb8c6f38897b40284196c0c9fac016
YuSung Lee <yl8402@nyu.edu>
2020-11-27 14:38:48 +0900

    Added customer class, removed HUD for now

--- sketch.js
+++ sketch.js
@@ -5,129 +5,41 @@ var recipe_noodle_button, recipe_noodle_textholder
 var steak_container, steak_button, steak_textholder
 var noodle_container, noodle_button, noodle_textholder
 
-
-
+//customer field
+var customers_list = []
+var astronaut, astronaut2, r2d2, puppy;
+var customerx,customerycustomerz;
+var customer_order, current_customer
+var customer_order_list = []
+var customer_hitbox;
 
 function setup() {
 	noCanvas();
 	world = new World('VRScene');
 
-	//Recipe Selection HUD
-	recipe_container = new Container3D({});
-
-	//Recipe button for Steak
-	recipe_steak_button = new Plane({
-    x:1,y:-0.75,z:-0.5,
-    red:255,green:0,blue:0,
-    width:1,height:0.5,depth:1,
-    clickFunction: function(me){
-
-			console.log("REcipe clicked");
-			steak_container.show()
-			recipe_container.hide();
 
 
-		}
-  })
-	recipe_steak_textholder = new Plane({
+	//customers
+	customerx = 3
+	customery = 1
+	customerz = 5
 
-			x: 1, y: -0.75, z: -0.5,
-			width: 1, height: 0.3,
-			red: 255, green: 255, blue: 255
+	r2d2 = new Customer("r2d2_obj","r2d2_mtl",0,-0.5,0,190,1,"r2d2")
+	puppy = new Customer("puppy_obj","puppy_mtl",0,0,0,40,3,"puppy")
+	astronaut = new Customer("astronaut_obj","astronaut_mtl",0,-1,0,-90,0.5,"astronaut")
+	astronaut2 = new Customer("astronaut2_obj","astronaut2_mtl",0,0,0,120,0.5,"astronaut2")
 
-	})
-	recipe_steak_textholder.tag.setAttribute('text','value: Check Steak; color: rgb(0,0,0); align: center;');
-	recipe_container.addChild(recipe_steak_button);
-	recipe_container.addChild(recipe_steak_textholder)
+	customers_list.push(r2d2)
+	customers_list.push(puppy)
+	customers_list.push(astronaut)
+	customers_list.push(astronaut2)
 
+	current_customer = random(customers_list);
+	current_customer.add_to_world()
 
-	//Recipe Button for Noodle
-	recipe_noodle_button = new Plane({
-    x:-1,y:-0.75,z:-0.5,
-    red:255,green:0,blue:0,
-    width:1,height:0.5,depth:1,
-    clickFunction: function(me){
-
-			console.log("REcipe clicked");
-			noodle_container.show()
-			recipe_container.hide();
-
-		}
-  })
-	recipe_noodle_textholder = new Plane({
-
-			x: -1, y: -0.75, z: -0.5,
-			width: 1, height: 0.3,
-			red: 255, green: 255, blue: 255
-
-	})
-	recipe_noodle_textholder.tag.setAttribute('text','value: Check Noodle; color: rgb(0,0,0); align: center;');
-	recipe_container.addChild(recipe_noodle_button);
-	recipe_container.addChild(recipe_noodle_textholder)
-
-	world.camera.cursor.addChild(recipe_container);
-	recipe_container.show();
-
-
-	//Steak Recipe HUD
-	steak_container = new Container3D({});
-	steak_button = new Plane({
-    x:1,y:0.5,z:-0.5,
-    red:255,green:0,blue:0,
-    width:1,height:1.5,depth:1,
-		clickFunction: function(me){
-
-			console.log("REcipe clicked");
-			steak_container.hide()
-			recipe_container.show();
-
-
-		}
-  })
-	steak_textholder = new Plane({
-
-			x: 1, y: 0.5, z: -0.5,
-			width: 1, height: 1,
-			red: 255, green: 255, blue: 255
-
-	})
-	steak_textholder.tag.setAttribute('text','value: Steak Recipe: \n\n 1. Get the steak from the fridge \n 2. Salt and paper the meat \n 3. Place the meat on the pan \n 4. Flip it between 9 - 12 seconds \n 5. Plate it after 7 - 10 after step 4. \n 6. Enjoy your delicious food :); color: rgb(0,0,0); align: center;');
-
-	steak_container.addChild(steak_button);
-	steak_container.addChild(steak_textholder)
-	world.camera.cursor.addChild(steak_container);
-	steak_container.hide();
-
-	//Noodle Recipe HUD
-	noodle_container = new Container3D({});
-	noodle_button = new Plane({
-    x:-1,y:0.5,z:-0.5,
-    red:255,green:0,blue:0,
-    width:1,height:1.5,depth:1,
-		clickFunction: function(me){
-
-			console.log("REcipe clicked");
-			noodle_container.hide()
-			recipe_container.show();
-
-
-		}
-  })
-	noodle_textholder = new Plane({
-
-			x: -1, y: 0.5, z: -0.5,
-			width: 1, height: 1,
-			red: 255, green: 255, blue: 255
-
-	})
-	noodle_textholder.tag.setAttribute('text','value: Noodle Recipe: \n\n 1. Get the pot at the sink \n 2. Pour water in the pot \n 3. Place the pot on the stove \n 4. Wait until it boils \n 5. Put noodle in the pot and wait 6-10 seconds \n 6. Enjoy your delicious food :); color: rgb(0,0,0); align: center;');
-
-	noodle_container.addChild(noodle_button);
-	noodle_container.addChild(noodle_textholder)
-	world.camera.cursor.addChild(noodle_container);
-	noodle_container.hide();
-
-	world.camera.cursor.show();
+	customer_order_list = ["Steak","Noodle","Sandwitch"]
+	customer_order = random(customer_order_list);
+	console.log(customer_order);
 
 
 	// // box primitive
@@ -140,7 +52,7 @@ function setup() {
 
 			}
 	});
-	world.add(ingredient_box);
+	//world.add(ingredient_box);
 	ingredient_box.rotateY(90)
 
 
@@ -169,7 +81,7 @@ function setup() {
 	});
 		world.add(sink_box);
 
-	var floor = new Plane({
+	var _floor = new Plane({
 		x:0, y:0, z:0,
 		width:100, height:100,
 		red:120, green:120, blue:120,
@@ -180,10 +92,116 @@ function setup() {
 	});
 
 	// add the plane to our world
-	world.add(floor);
+	world.add(_floor);
 }
 
 function draw() {
-	if(mouseIsPressed){
+}
+class Customer{
+
+	constructor(_asset, _mtl, x_align,y_align,z_align,_rotationY,_scale,_name){
+		this.container = new Container3D({
+			y: -10
+		})
+		this.name = _name
+		this.customer = new OBJ({
+			asset: _asset,
+			mtl: _mtl,
+			x: customerx + x_align,
+			y: customery + y_align,
+			z: customerz + z_align,
+			rotationX: 0,
+			rotationY: _rotationY,
+			scaleX: _scale,
+			scaleY: _scale,
+			scaleZ: _scale,
+		});
+
+		this.hitbox = new Plane({
+			x: customerx + x_align -0.5,
+			y: customery,
+			z: customerz + z_align,
+			rotationX: 0,
+			rotationY: 90,
+			scaleX: 1,
+			scaleY: 1,
+			scaleZ: 1,
+
+			side:'double',
+			clickFunction: function(me){
+
+				console.log(customer_order);
+			}
+		})
+
+		this.serve_button = new Plane({
+			x: customerx + x_align -0.5,
+			y: customery,
+			z: customerz + z_align + 1,
+			red:0,green:120,blue:0,
+			rotationX: 0,
+			rotationY: 270,
+			scaleX: 1,
+			scaleY: 0.5,
+			scaleZ: 1,
+
+			side:'double',
+			clickFunction: function(me){
+
+				console.log("you served " + customer_order);
+			}
+		})
+		this.serve_button.tag.setAttribute('text','value: Serve the Food; color: rgb(255,255,255); align: center;');
+
+		this.kickout_button = new Plane({
+			x: customerx + x_align -0.5,
+			y: customery,
+			z: customerz + z_align-1,
+			red:120,green:0,blue:0,
+
+			rotationX: 0,
+			rotationY: 270,
+			scaleX: 1,
+			scaleY: 0.5,
+			scaleZ: 1,
+
+			side:'double',
+			clickFunction: function(me){
+
+				console.log("Kicked out the customer");
+				//current_customer.remove_from_world()
+				current_customer.remove_from_world()
+				let prev_customer = current_customer
+				while(prev_customer == current_customer){
+					current_customer = random(customers_list);
+				}
+				current_customer.add_to_world()
+
+			}
+
+		})
+
+		this.kickout_button.tag.setAttribute('text','value: Kick Out!; color: rgb(255,255,255); align: center;');
+
+		this.container.addChild(this.customer)
+		this.container.addChild(this.hitbox)
+		this.container.addChild(this.serve_button)
+		this.container.addChild(this.kickout_button)
+		this.hitbox.hide()
+		world.add(this.container)
+
 	}
+	//constructor ends
+
+	add_to_world(){
+		customer_order = random(customer_order_list)
+		this.container.setY(0)
+
+	}
+
+	remove_from_world(){
+		this.container.setY(-10)
+
+	}
+
 }
